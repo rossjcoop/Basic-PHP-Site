8f026b07705381309730031794ef2c7584b1b99f
Ross Cooper <rossjamescooper@gmail.com>
2019-10-04 15:38:00 -0700

    Added rand function to display 4 iitems on home page randomly

--- index.php
+++ index.php
@@ -14,8 +14,9 @@ include("inc/header.php"); ?>
 
 				<ul class="items">
 					<?php 
-					foreach($catalog as $id => $item) {
-						echo get_item_html($id,$item);
+					$random = array_rand($catalog,4);
+					foreach($random as $id) {
+						echo get_item_html($id,$catalog[$id]);
 					}
 					?>
 					<!-- <li><a href="details.php?id=201"><img src="img/media/forest_gump.jpg" alt="Forrest Gump"><p>View Details</p></a></li><li><a href="details.php?id=204"><img src="img/media/princess_bride.jpg" alt="The Princess Bride"><p>View Details</p></a></li><li><a href="details.php?id=302"><img src="img/media/elvis_presley.jpg" alt="Elvis Forever"><p>View Details</p></a></li><li><a href="details.php?id=303"><img src="img/media/garth_brooks.jpg" alt="No Fences"><p>View Details</p></a></li> -->								
