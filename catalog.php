09ac8843addf9ebcc6bd08f793f9371a323e3f41
Ross Cooper <rossjamescooper@gmail.com>
2019-10-04 15:27:34 -0700

    Added function to loop through data

--- catalog.php
+++ catalog.php
@@ -1,5 +1,6 @@
 <?php
 include("inc/data.php");
+include("inc/functions.php");
 
 $pageTitle = 'Full Catalog';
 $section = null;
@@ -25,12 +26,9 @@ include("inc/header.php"); ?>
 		<h1><?php echo $pageTitle; ?></h1>
 
 		<ul class="items">
-			<?php foreach($catalog as $item) {
-				echo "<li><a href='#'><img src='" 
-					. $item["img"] . "' alt='" 
-					. $item["title"] . "'/>" 
-					. "<p>View Details</p>"
-					. "</a></li>";
+			<?php 
+			foreach($catalog as $id => $item) {
+				echo get_item_html($id,$item);
 			}
 			?>
 		</ul>
