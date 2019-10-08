46e27e4450d77ee110dadd223dbb7e9d2f80bc87
Ross Cooper <rossjamescooper@gmail.com>
2019-10-08 15:23:05 -0700

    Added suggestion form

--- suggest.php
+++ suggest.php
@@ -5,7 +5,26 @@ $section = "suggest";
 include("inc/header.php"); ?>
 
 <div class="section page">
-	<h1>Suggest a Media Item</h1>
+	<div class="wrapper">
+		<h1>Suggest a Media Item</h1>
+		<p>If you think there is something I&rsquo;m missing, let me know! Complete the form to send me an email.</p>
+		<form method="post">
+			<table>
+				<tr>
+					<th><label for="name">Name</label></th>
+					<td><input type="text" name="name" id="name"/></td>
+				</tr>
+				<tr>
+					<th><label for="email">Email</label></th>
+					<td><input type="text" name="email" id="email"/></td>
+				</tr>
+				<tr>
+					<th><label for="details">Suggest Item Details</label></th>
+					<td><textarea name="details" id="details"></textarea></td>
+				</tr>
+			</table>
+			<input type="submit" name="" value="Send"/>
+		</form>
 </div>
 
 
