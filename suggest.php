4ed6119d61b471918c0edb136407fa2f5f15c67c
Ross Cooper <rossjamescooper@gmail.com>
2019-10-10 09:35:07 -0700

    Consolidated files into suggest, added form validation

--- suggest.php
+++ suggest.php
@@ -1,4 +1,35 @@
 <?php 
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	$name = trim(filter_input(INPUT_POST, "name", FILTER_SANITIZE_STRING));
+	$email = trim(filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL));
+	$details = trim(filter_input(INPUT_POST, "details", FILTER_SANITIZE_SPECIAL_CHARS));
+
+	if ($name == "" || $email == "" || $details == "") {
+		echo "Please fill in the required fields: Name, Email, and Details";
+		exit;
+	}
+
+	if ($_POST["validation"] != "") {
+		echo "Bad form input";
+		exit;
+	}
+
+	echo "<pre>";
+	$email_body = "";
+	$email_body .= "Name " . $name . "\n";
+	$email_body .= "Email " . $email . "\n";
+	$email_body .= "Details " . $details . "\n";
+	echo $email_body;
+	echo "</pre>";
+
+	echo $details;
+
+	//To Do: Send email
+	header("location:suggest.php?status=thanks");
+
+}
+
 $pageTitle = 'Suggest a Media Item';
 $section = "suggest";
 
@@ -7,8 +38,13 @@ include("inc/header.php"); ?>
 <div class="section page">
 	<div class="wrapper">
 		<h1>Suggest a Media Item</h1>
+
+		<?php if (isset($_GET["status"]) && $_GET["status"] == "thanks") {
+			echo "<p>Thanks for the email! I&rsquo;ll check out your suggestion shortly!</p>";
+		} else { ?>
+
 		<p>If you think there is something I&rsquo;m missing, let me know! Complete the form to send me an email.</p>
-		<form method="post" action="process.php">
+		<form method="post" action="suggest.php">
 			<table>
 				<tr>
 					<th><label for="name">Name</label></th>
@@ -22,9 +58,15 @@ include("inc/header.php"); ?>
 					<th><label for="details">Suggest Item Details</label></th>
 					<td><textarea name="details" id="details"></textarea></td>
 				</tr>
+				<tr style="display:none">
+					<td><input type="text" name="validation" id="validation"/>
+					<p>Please leave this field blank</p></td>
+				</tr>
 			</table>
 			<input type="submit" name="" value="Send"/>
-		</form>
+		</form>";
+		<?php } ?>
+	</div>
 </div>
 
 
